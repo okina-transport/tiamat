344f940736b93cf5d1a271f3e303018d922ccc28
Grégoire Fora <gfora@okina.fr>
2018-03-02 09:56:40 +0100

    trying to debug pook leak

--- src/main/java/org/rutebanken/tiamat/health/DbStatusChecker.java
+++ src/main/java/org/rutebanken/tiamat/health/DbStatusChecker.java
@@ -17,26 +17,22 @@ package org.rutebanken.tiamat.health;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityManager;
-@Service
+//@Service
 public class DbStatusChecker {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
-    private EntityManager entityManager;
+//    @Autowired
+//    private EntityManager entityManager;
 
     public boolean isDbUp() {
-        try {
-            if ((Integer) entityManager.createNativeQuery("SELECT 1").getSingleResult() == 1) {
-                return true;
-            }
-        } catch (RuntimeException e) {
-            logger.warn("Failed while testing DB connection", e);
-        }
+//        try {
+//            if ((Integer) entityManager.createNativeQuery("SELECT 1").getSingleResult() == 1) {
+//                return true;
+//            }
+//        } catch (RuntimeException e) {
+//            logger.warn("Failed while testing DB connection", e);
+//        }
         return false;
     }
 }

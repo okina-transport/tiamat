ad52641a6087ad10e6642144a6a235166b03a644
Grégoire Fora <gfora@okina.fr>
2019-11-05 13:37:47 +0100

    Delete test export idfm

--- src/test/java/org/rutebanken/tiamat/exporter/AsyncPublicationDeliveryExporterTest.java
+++ src/test/java/org/rutebanken/tiamat/exporter/AsyncPublicationDeliveryExporterTest.java
@@ -84,44 +84,4 @@ public class AsyncPublicationDeliveryExporterTest extends TiamatIntegrationTest 
             }
         }
     }
-
-//    @Ignore
-    @Test
-    public void testExportIdfm() throws JAXBException, ParserConfigurationException, IOException, SAXException, TransformerException, XMLStreamException, InterruptedException {
-
-        StopPlace stopPlace = new StopPlace(new EmbeddableMultilingualString("stop place to be exported"));
-        Quay quay = new Quay(new EmbeddableMultilingualString("quay to be exported"));
-        HashSet<Quay> quays = new HashSet<>();
-        quays.add(quay);
-        stopPlace.setQuays(quays);
-
-        stopPlaceRepository.save(stopPlace);
-        stopPlaceRepository.flush();
-
-        ExportParams exportParams = newExportParamsBuilder().setStopPlaceSearch(new StopPlaceSearch()).build();
-
-        ExportJob exportJob = asyncPublicationDeliveryExporter.startExportJob(exportParams);
-
-        assertThat(exportJob.getId()).isGreaterThan(0L);
-
-        long start = System.currentTimeMillis();
-        long timeout = 10000;
-        while (true) {
-            Optional<ExportJob> actualExportJob = exportJobRepository.findById(exportJob.getId());
-            if (actualExportJob.get().getStatus().equals(exportJob.getStatus())) {
-                if (System.currentTimeMillis() - start > timeout) {
-                    fail("Waited more than " + timeout + " millis for job status to change");
-                }
-                Thread.sleep(1000);
-                continue;
-            }
-
-            if (actualExportJob.get().getStatus().equals(JobStatus.FAILED)) {
-                fail("Job status is failed");
-            } else if (actualExportJob.get().getStatus().equals(JobStatus.FINISHED)) {
-                System.out.println("Job finished");
-
-            }
-        }
-    }
 }

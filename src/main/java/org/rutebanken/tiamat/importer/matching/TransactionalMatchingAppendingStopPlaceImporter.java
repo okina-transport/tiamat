64ab88eceb35ec0046922a5f24122a1c6158ed6f
Grégoire Fora <gfora@okina.fr>
2020-04-23 08:41:52 +0200

    Desactivation nearbyStopPlaceFinder

--- src/main/java/org/rutebanken/tiamat/importer/matching/TransactionalMatchingAppendingStopPlaceImporter.java
+++ src/main/java/org/rutebanken/tiamat/importer/matching/TransactionalMatchingAppendingStopPlaceImporter.java
@@ -150,13 +150,13 @@ public class TransactionalMatchingAppendingStopPlaceImporter {
 
             foundStopPlaces = filteredStopPlaces;
         }
-
-        if (foundStopPlaces.isEmpty()) {
-            org.rutebanken.tiamat.model.StopPlace nearbyStopPlace = nearbyStopPlaceFinder.find(incomingStopPlace, ALLOW_OTHER_TYPE_AS_ANY_MATCH, provider, noMergeIDFMStopPlaces);
-            if (nearbyStopPlace != null) {
-                foundStopPlaces = Arrays.asList(nearbyStopPlace);
-            }
-        }
+        // Désactiver pour Mosaic car fusionne des pa au nom proche au sein d'un même pa commercial
+//        if (foundStopPlaces.isEmpty()) {
+//            org.rutebanken.tiamat.model.StopPlace nearbyStopPlace = nearbyStopPlaceFinder.find(incomingStopPlace, ALLOW_OTHER_TYPE_AS_ANY_MATCH, provider, noMergeIDFMStopPlaces);
+//            if (nearbyStopPlace != null) {
+//                foundStopPlaces = Arrays.asList(nearbyStopPlace);
+//            }
+//        }
 
         if (foundStopPlaces.isEmpty()) {
             logger.warn("Cannot find stop place from IDs or location: {}. StopPlace toString: {}. Will add it as a new one",

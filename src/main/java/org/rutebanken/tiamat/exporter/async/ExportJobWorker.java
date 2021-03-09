08775bc9b78e34b5b03559473ea56b7d60aa5495
gcarrere <gcarrere@okina.fr>
2021-03-09 16:04:32 +0100

    netex arrets
    Signed-off-by: gcarrere <gcarrere@okina.fr>

--- src/main/java/org/rutebanken/tiamat/exporter/async/ExportJobWorker.java
+++ src/main/java/org/rutebanken/tiamat/exporter/async/ExportJobWorker.java
@@ -85,7 +85,9 @@ public class ExportJobWorker implements Runnable {
 
     public void run() {
         logger.info("Started export job: {}", exportJob);
-        final File localExportZipFile = new File(localExportPath + File.separator + provider.name+ File.separator+exportJob.getFileName());
+
+        File providerDir = new File(localExportPath + File.separator + provider.name);
+        final File localExportZipFile = new File(providerDir, File.separator+exportJob.getFileName());
         File localExportXmlFile = new File(localExportPath + File.separator + fileNameWithoutExtention + ".xml");
         try {
 

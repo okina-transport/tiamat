e08f66ed4956ab8458a08e6a4f47dd9287c24204
gcarrere <gcarrere@okina.fr>
2021-10-31 14:45:59 +0100

    Fix TU
    Signed-off-by: gcarrere <gcarrere@okina.fr>

--- src/main/java/org/rutebanken/tiamat/config/H2Functions.java
+++ src/main/java/org/rutebanken/tiamat/config/H2Functions.java
@@ -55,11 +55,6 @@ public class H2Functions implements InitializingBean {
                 logger.info("H2. Creating alias to method generate_series");
                 jdbcTemplate.execute("CREATE ALIAS IF NOT EXISTS generate_series FOR \"org.rutebanken.tiamat.config.H2Functions.generateSeries\"");
 
-                logger.info("H2. Creating alias to method ST_WITHIN");
-                jdbcTemplate.execute("CREATE ALIAS ST_Within FOR \"geodb.GeoDB.ST_Within\"");
-
-                logger.info("H2. Creating alias to method ST_CONTAINS");
-                jdbcTemplate.execute("CREATE ALIAS ST_Contains FOR \"geodb.GeoDB.ST_Contains\"");
             }
         } catch (SQLException sqlException) {
             logger.warn("Cannot create h2 aliases", sqlException);

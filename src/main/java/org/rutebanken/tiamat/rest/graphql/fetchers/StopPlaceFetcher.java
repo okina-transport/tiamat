b423beb520a608587df833c81caf551ffadf5d9c
Grégoire Fora <gfora@okina.fr>
2022-11-25 12:17:19 +0100

    StopPlaceFetcher desactivation filter by user orgs

--- src/main/java/org/rutebanken/tiamat/rest/graphql/fetchers/StopPlaceFetcher.java
+++ src/main/java/org/rutebanken/tiamat/rest/graphql/fetchers/StopPlaceFetcher.java
@@ -251,11 +251,11 @@ class StopPlaceFetcher implements DataFetcher {
         }
 
         // Remove SP not belonging to user orgs
-        if (!stopPlacesPage.getContent().isEmpty() && !isUserAdmin) {
-            // TODO surement à revoir à tester
-            List<StopPlace> userOrgFilteredStopPlaces = stopPlacesPage.getContent().stream().filter(stopPlace -> userOrgs.contains(stopPlace.getProvider())).collect(Collectors.toList());
-            stopPlacesPage = new PageImpl<>(userOrgFilteredStopPlaces, new PageRequest(environment.getArgument(PAGE), environment.getArgument(SIZE)), 1L);
-        }
+        // TODO surement à revoir à tester
+//        if (!stopPlacesPage.getContent().isEmpty() && !isUserAdmin) {
+//            List<StopPlace> userOrgFilteredStopPlaces = stopPlacesPage.getContent().stream().filter(stopPlace -> userOrgs.contains(stopPlace.getProvider())).collect(Collectors.toList());
+//            stopPlacesPage = new PageImpl<>(userOrgFilteredStopPlaces, new PageRequest(environment.getArgument(PAGE), environment.getArgument(SIZE)), 1L);
+//        }
 
         final List<StopPlace> stopPlaces = stopPlacesPage.getContent();
         boolean onlyMonomodalStopplaces = false;

fe76836f49282552fc6cab684cc099d625e3359e
gcarrere <gcarrere@okina.fr>
2024-04-07 14:57:06 +0200

    Fix_TU

--- src/main/java/org/rutebanken/tiamat/netex/mapping/mapper/QuayMapper.java
+++ src/main/java/org/rutebanken/tiamat/netex/mapping/mapper/QuayMapper.java
@@ -68,7 +68,7 @@ public class QuayMapper extends CustomMapper<Quay, org.rutebanken.tiamat.model.Q
         }
 
         if (quay.getPrivateCode() != null && quay.getPrivateCode().getValue() != null) {
-            quay2.setPrivateCode(new org.rutebanken.tiamat.model.PrivateCodeStructure(quay.getPrivateCode().getValue(), null));
+            quay2.setPrivateCode(new org.rutebanken.tiamat.model.PrivateCodeStructure(quay.getPrivateCode().getValue(), quay.getPrivateCode().getType()));
         }
     }
 

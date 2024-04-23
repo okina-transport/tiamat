7d948d5a91d80dd1eae280b7caef7792e02c9298
poswald <poswald@okina.fr>
2024-04-23 09:18:39 +0200

    feat : DATA-88 add TU

---
+++ src/test/java/org/rutebanken/tiamat/importer/manualImports/PoiNetexImportTest.java
@@ -0,0 +1,61 @@
+package org.rutebanken.tiamat.importer.manualImports;
+
+import org.junit.Test;
+import org.rutebanken.netex.model.PublicationDeliveryStructure;
+import org.rutebanken.tiamat.TiamatIntegrationTest;
+import org.rutebanken.tiamat.importer.PoisImporter;
+import org.rutebanken.tiamat.rest.exception.TiamatBusinessException;
+import org.rutebanken.tiamat.rest.netex.publicationdelivery.PublicationDeliveryUnmarshaller;
+import org.rutebanken.tiamat.rest.parkingsNetex.ImportParkingsNetexResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.xml.sax.SAXException;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Transactional
+//Dirties context is used to clear H2 database before each test
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class PoiNetexImportTest extends TiamatIntegrationTest {
+
+    @Autowired
+    public ImportParkingsNetexResource importResource;
+
+    @Autowired
+    private PublicationDeliveryUnmarshaller publicationDeliveryUnmarshaller;
+
+    @Autowired
+    private PoisImporter poisImporter;
+
+    @Test
+    public void testPoiNetex() throws IOException, JAXBException, SAXException {
+        launchImportForFile("poi.xml");
+    }
+
+    @Test
+    public void testPoiNetexWithoutName() throws IOException, JAXBException, SAXException {
+        launchImportForFile("poi_without_name.xml");
+    }
+
+    /**
+     * Launch a manual import of the file given as parameter
+     * @param fileName
+     * the file to import
+     * @throws IOException
+     */
+    private void launchImportForFile(String fileName) throws IOException, JAXBException, SAXException {
+        File file = new File("src/test/resources/manualImports/poiNetex/" + fileName);
+
+        try (InputStream testInputStream = new FileInputStream(file)) {
+            PublicationDeliveryStructure incomingPublicationDelivery = publicationDeliveryUnmarshaller.unmarshal(testInputStream);
+            poisImporter.importPointOfInterests(incomingPublicationDelivery, null);
+        } catch (TiamatBusinessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}

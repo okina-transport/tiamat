001e12232fcc47176e14b10a4d88b83cb8afcff5
afreche <alexandre.freche@lumiplan.com>
2025-01-27 11:11:11 +0100

    fix: update version retrieval

--- build.sh
+++ build.sh
@@ -2,17 +2,11 @@
 
 echo Building docker image
 
-# Back
-VERSION=$(mvn -q \
-    -Dexec.executable=echo \
-    -Dexec.args='${project.version}' \
-    --non-recursive \
-    exec:exec)
-IMAGE_NAME=registry.okina.fr/mobiiti/tiamat:"${VERSION}"
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
 
-echo version:${VERSION}
-echo targetFile:target/tiamat-${VERSION}.jar
+echo "version:${VERSION}"
+echo "targetFile:target/tiamat-${VERSION}.jar"
 
 #mvn spring-boot:build-image -Dspring-boot.build-image.imageName="${IMAGE_NAME}" -Dfile.encoding=UTF-8
-docker build -t "${IMAGE_NAME}" --build-arg JAR_FILE=target/tiamat-${VERSION}.jar .
+docker build -t "${IMAGE_NAME}" --build-arg JAR_FILE=target/tiamat-"${VERSION}".jar .
 docker push "${IMAGE_NAME}"

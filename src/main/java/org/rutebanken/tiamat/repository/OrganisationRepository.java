f97b0e07b273963b19ddaf8befad24e7eea4f0c9
afreche <alexandre.freche@lumiplan.com>
2026-02-18 15:34:13 +0100

    fix(): do not use feign for MDM client use @HttpExchange instead

--- src/main/java/org/rutebanken/tiamat/repository/OrganisationRepository.java
+++ src/main/java/org/rutebanken/tiamat/repository/OrganisationRepository.java
@@ -4,7 +4,10 @@ import org.rutebanken.tiamat.model.Organisation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrganisationRepository extends JpaRepository<Organisation, Long>, EntityInVersionRepository<Organisation> {
 
+    Optional<Organisation> findByName(String name);
 }

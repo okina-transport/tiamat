1985d9d9e2f818b222394adb6abb51ef0dd9dca4
afreche <alexandre.freche@lumiplan.com>
2026-03-03 16:18:37 +0100

    feat(): log exceptions caught by ExceptionMappers

--- src/main/java/org/rutebanken/tiamat/rest/exception/NoSuchElementMapper.java
+++ src/main/java/org/rutebanken/tiamat/rest/exception/NoSuchElementMapper.java
@@ -18,11 +18,18 @@ package org.rutebanken.tiamat.rest.exception;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.NoSuchElementException;
 
 @Provider
 public class NoSuchElementMapper implements ExceptionMapper<NoSuchElementException> {
+    private static final Logger log = LoggerFactory.getLogger(NoSuchElementMapper.class);
+
     public Response toResponse(NoSuchElementException ex) {
+        log.error("Exception caught", ex);
+
         return Response.status(Response.Status.NOT_FOUND)
                 .entity(ex)
                 .build();

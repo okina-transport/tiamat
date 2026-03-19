08e9a69214389ca9112fa3a1abafae21a30004a7
jmarzin1 <julien.marzin@lumiplan.com>
2026-03-19 14:51:24 +0100

    fix(): point of interest import

--- src/main/java/org/rutebanken/tiamat/exporter/StreamingPublicationDelivery.java
+++ src/main/java/org/rutebanken/tiamat/exporter/StreamingPublicationDelivery.java
@@ -871,7 +871,7 @@ public class StreamingPublicationDelivery {
                 listMembers.addAll(refToTypeOfPaymentMethod.values());
             }
 
-            logger.info("Adding {} typesOfParking in generalFrame");
+            logger.info("Adding typesOfParking in generalFrame");
         } else {
             logger.info("No parkings to export");
         }

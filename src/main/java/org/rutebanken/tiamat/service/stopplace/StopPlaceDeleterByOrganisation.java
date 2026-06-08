a212165675543d7817f5723adc72b14ecc23e431
gcarrere <gcarrere@okina.fr>
2026-06-08 16:33:56 +0200

    DATA-1944 Handle object delete in MDM

--- src/main/java/org/rutebanken/tiamat/service/stopplace/StopPlaceDeleterByOrganisation.java
+++ src/main/java/org/rutebanken/tiamat/service/stopplace/StopPlaceDeleterByOrganisation.java
@@ -1,6 +1,7 @@
 package org.rutebanken.tiamat.service.stopplace;
 
 import org.rutebanken.tiamat.auth.UsernameFetcher;
+import org.rutebanken.tiamat.importer.mdm.MdmService;
 import org.rutebanken.tiamat.lock.MutateLock;
 import org.rutebanken.tiamat.repository.StopPlaceRepository;
 import org.slf4j.Logger;
@@ -14,21 +15,25 @@ public class StopPlaceDeleterByOrganisation {
 
     private static final Logger logger = LoggerFactory.getLogger(StopPlaceDeleterByOrganisation.class);
 
-    @Autowired
-    private StopPlaceRepository stopPlaceRepository;
 
-    @Autowired
-    private UsernameFetcher usernameFetcher;
+    private final StopPlaceRepository stopPlaceRepository;
+    private final UsernameFetcher usernameFetcher;
+    private final MutateLock mutateLock;
+    private final MdmService mdmService;
 
-    @Autowired
-    private MutateLock mutateLock;
+    public StopPlaceDeleterByOrganisation(StopPlaceRepository stopPlaceRepository, UsernameFetcher usernameFetcher, MutateLock mutateLock, MdmService mdmService) {
+        this.stopPlaceRepository = stopPlaceRepository;
+        this.usernameFetcher = usernameFetcher;
+        this.mutateLock = mutateLock;
+        this.mdmService = mdmService;
+    }
 
     public boolean deleteStopPlacesByOrganisation(String organisation) {
 
         return mutateLock.executeInLock(() -> {
             String usernameForAuthenticatedUser = usernameFetcher.getUserNameForAuthenticatedUser();
             logger.warn("About to delete stop place by organisation {}. User: {}", organisation, usernameForAuthenticatedUser);
-
+            mdmService.deleteStopPlaceAndQuayIdsByDataset(organisation);
             return stopPlaceRepository.deleteAllStopPlacesQuaysByOrganisation(organisation);
         });
     }

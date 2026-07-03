45e2c7c5e7ee2b639748b25ea1e0bc0fef46d350
afreche <alexandre.freche@lumiplan.com>
2026-07-03 10:44:03 +0200

    feat(DATA-1986): log user actions in TIAMAT
    * add LoggingService to log user actions (send logs to ACTIVEMQ)
    * log Quay/StopPlace/Parking CREATION/UPDATE/DELETE
    * imports (GBFS parking, POI, etc ...)

--- src/main/java/org/rutebanken/tiamat/rest/poiNetex/ImportPointOfInterestsNetexResource.java
+++ src/main/java/org/rutebanken/tiamat/rest/poiNetex/ImportPointOfInterestsNetexResource.java
@@ -1,8 +1,17 @@
 package org.rutebanken.tiamat.rest.poiNetex;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.xml.bind.JAXBException;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.rutebanken.netex.model.PublicationDeliveryStructure;
+import org.rutebanken.tiamat.auth.UsernameFetcher;
+import org.rutebanken.tiamat.changelog.LoggingService;
 import org.rutebanken.tiamat.general.ImportJobWorker;
 import org.rutebanken.tiamat.general.ImportJobWorkerBuilder;
 import org.rutebanken.tiamat.general.PointOfInterestCSVHelper;
@@ -18,13 +27,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.xml.sax.SAXException;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.xml.bind.JAXBException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -43,13 +45,17 @@ public class ImportPointOfInterestsNetexResource {
     private final ImportJobWorkerBuilder importJobWorkerBuilder;
     private final PointOfInterestCSVHelper poiHelper;
     private final JobRepository jobRepository;
+    private final LoggingService loggingService;
+    private final UsernameFetcher usernameFetcher;
 
 
-    public ImportPointOfInterestsNetexResource(PublicationDeliveryUnmarshaller publicationDeliveryUnmarshaller, ImportJobWorkerBuilder importJobWorkerBuilder, PointOfInterestCSVHelper poiHelper, JobRepository jobRepository) {
+    public ImportPointOfInterestsNetexResource(PublicationDeliveryUnmarshaller publicationDeliveryUnmarshaller, ImportJobWorkerBuilder importJobWorkerBuilder, PointOfInterestCSVHelper poiHelper, JobRepository jobRepository, LoggingService loggingService, UsernameFetcher usernameFetcher) {
         this.publicationDeliveryUnmarshaller = publicationDeliveryUnmarshaller;
         this.importJobWorkerBuilder = importJobWorkerBuilder;
         this.poiHelper = poiHelper;
         this.jobRepository = jobRepository;
+        this.loggingService = loggingService;
+        this.usernameFetcher = usernameFetcher;
     }
 
     @PreAuthorize("@rolesChecker.hasRoleEdit()")
@@ -63,6 +69,7 @@ public class ImportPointOfInterestsNetexResource {
                                        @FormDataParam("folder") String folder)
             throws IOException, IllegalArgumentException, JAXBException, SAXException {
         logger.info("Received POI Netex publication delivery, starting to parse...");
+        loggingService.logPoiNetexImport(usernameFetcher.getUserNameForAuthenticatedUser(), fileName);
         PublicationDeliveryStructure incomingPublicationDelivery = publicationDeliveryUnmarshaller.unmarshal(inputStream);
         poiHelper.clearClassificationCache();
         try {
@@ -87,6 +94,7 @@ public class ImportPointOfInterestsNetexResource {
                                             @FormDataParam("folder") String folder)
             throws IOException, IllegalArgumentException {
         logger.info("Lancement de l'import POI netex pour le fichier: " + fileName);
+        loggingService.logPoiNetexImport(usernameFetcher.getUserNameForAuthenticatedUser(), fileName);
 
         Job job = new Job();
         job.setFileName(fileName);

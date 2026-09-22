ed964840e2fb9d33d963e2c9489efa30c68a50ba
gcarrere <gcarrere@okina.fr>
2026-09-22 15:02:17 +0200

    Revert "fix: DATA-2161 accessibility issue when creating multimodal stop"
    This reverts commit 57723c400b45fcc0afefa14d00eede37f3463b7b.

--- src/main/java/org/rutebanken/tiamat/versioning/save/StopPlaceVersionedSaverService.java
+++ src/main/java/org/rutebanken/tiamat/versioning/save/StopPlaceVersionedSaverService.java
@@ -55,9 +55,12 @@ import static org.rutebanken.tiamat.versioning.save.DefaultVersionedSaverService
 @Service
 public class StopPlaceVersionedSaverService {
 
-    public static final int ADJACENT_STOP_PLACE_MAX_DISTANCE_IN_METERS = 30;
-    public static final InterchangeWeightingEnumeration DEFAULT_WEIGHTING = InterchangeWeightingEnumeration.INTERCHANGE_ALLOWED;
     private static final Logger logger = LoggerFactory.getLogger(StopPlaceVersionedSaverService.class);
+
+    public static final int ADJACENT_STOP_PLACE_MAX_DISTANCE_IN_METERS = 30;
+
+    public static final InterchangeWeightingEnumeration DEFAULT_WEIGHTING = InterchangeWeightingEnumeration.INTERCHANGE_ALLOWED;
+
     @Autowired
     private ZoneDistanceChecker zoneDistanceChecker;
 
@@ -159,7 +162,7 @@ public class StopPlaceVersionedSaverService {
             newVersion.setTariffZones(newVersion.getTariffZones().stream()
                     .map(tariffZoneRef -> {
                         TariffZone tariffZone = tariffZoneRepository.findFirstByNetexIdOrderByVersionDesc(tariffZoneRef.getRef());
-                        if (tariffZone == null) {
+                        if(tariffZone == null){
                             tariffZone = resolve(tariffZoneRef);
                         }
                         if (tariffZone == null) {
@@ -176,7 +179,7 @@ public class StopPlaceVersionedSaverService {
 
         Instant changed = Instant.now();
 
-        if (optimizeAccessibilityAssessmentsStopPlace) {
+        if(optimizeAccessibilityAssessmentsStopPlace){
             logger.debug("Rearrange accessibility assessments for: {}", newVersion);
             accessibilityAssessmentOptimizer.optimizeAccessibilityAssessmentsStopPlace(newVersion);
         }
@@ -218,14 +221,13 @@ public class StopPlaceVersionedSaverService {
                 tariffZonesLookupService.populateTariffZone(child);
             });
 
-            Set<StopPlace> savedChildren = newVersion.getChildren().stream()
-                    .map(stopPlaceRepository::removeImportedIdAndSave)
-                    .collect(Collectors.toSet());
-            newVersion.setChildren(savedChildren);
+            for (StopPlace child : newVersion.getChildren()) {
+                stopPlaceRepository.removeImportedIdAndSave(child);
+            }
             if (logger.isDebugEnabled()) {
                 logger.debug("Saved children: {}", newVersion.getChildren().stream()
-                        .map(sp -> "{id:" + sp.getId() + " netexId:" + sp.getNetexId() + " version:" + sp.getVersion() + "}")
-                        .collect(Collectors.toList()));
+                                                           .map(sp -> "{id:" + sp.getId() + " netexId:" + sp.getNetexId() + " version:" + sp.getVersion() + "}")
+                                                           .collect(Collectors.toList()));
             }
         }
         mdmService.generateIdentifier(newVersion);
@@ -273,8 +275,8 @@ public class StopPlaceVersionedSaverService {
     }
 
 
-    private void updateValidBetweenInChildren(StopPlace stopPlace, ValidBetween validBetween) {
-        if (stopPlace.getChildren() == null) {
+    private void updateValidBetweenInChildren(StopPlace stopPlace, ValidBetween validBetween){
+        if (stopPlace.getChildren() == null){
             return;
         }
 
@@ -283,14 +285,14 @@ public class StopPlaceVersionedSaverService {
         }
     }
 
-    private void terminateChild(StopPlace stopPlaceToTerminate, Instant terminationInstant) {
-        if (stopPlaceToTerminate.getChildren() != null) {
+    private void terminateChild(StopPlace stopPlaceToTerminate, Instant terminationInstant ){
+        if (stopPlaceToTerminate.getChildren() != null){
 
             for (StopPlace child : stopPlaceToTerminate.getChildren()) {
                 ValidBetween validBetween;
-                if (child.getValidBetween() != null) {
+                if (child.getValidBetween() != null){
                     validBetween = child.getValidBetween();
-                } else {
+                }else{
                     validBetween = new ValidBetween();
                     validBetween.setFromDate(terminationInstant.minusMillis(MILLIS_BETWEEN_VERSIONS));
                     child.setValidBetween(validBetween);
@@ -302,7 +304,7 @@ public class StopPlaceVersionedSaverService {
 
     private void validateAdjacentSites(StopPlace newVersion) {
         if (newVersion.getAdjacentSites() != null) {
-            logger.info("Validating adjacent sites for {} {}", newVersion.getNetexId(), newVersion.getName());
+                logger.info("Validating adjacent sites for {} {}", newVersion.getNetexId(), newVersion.getName());
             for (SiteRefStructure siteRefStructure : newVersion.getAdjacentSites()) {
 
                 if (newVersion.getNetexId() != null && (newVersion.getNetexId().equals(siteRefStructure.getRef()))) {
